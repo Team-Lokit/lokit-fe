9162278d58e38c31a86cc7063b05a5e05dd76c2d
HongJu Lee <hongju4385@gmail.com>
2026-01-21 00:52:03 +0900

    feat: 팝업 컴포넌트 추가 (#19)
    * refactor: rest props 전달로 버튼 확장성 개선
    
    * refactor: 플로팅버튼에 backdrop-filter 추가
    
    * feat: theme에 z-index 스타일 추가
    
    * feat: 팝업 컴포넌트 및 스타일 추가
    
    * feat: 모달 컴포넌트 및 스타일 추가
    
    * feat: 오버레이 컴포넌트 및 스타일 추가
    
    * feat: Escape 키 닫기 훅 추가
    
    * feat: 스크롤 잠금 훅 추가
    
    * feat: 팝업 훅(모달, 오버레이 공통) 추가
    
    * test: 모달 컴포넌트에 대한 스토리북 추가
    
    * test: 오버레이 컴포넌트에 대한 스토리북 추가
    
    * refactor: useESC keyup -> keydown으로 수정
    
    * refactor: body가 아닌 modal-root에 팝업이 나오도록 수정
    
    * refactor: zIndex 설정 값 변경
    
    * refactor: 모달, 오버레이 타입 선언
    
    * style: format:styles

---
+++ apps/web/src/components/popup/overlay/Overlay.tsx
@@ -0,0 +1,50 @@
+import { ReactElement, ReactNode } from 'react';
+import PopupRoot from '../PopupRoot';
+import PopupLayer from '../PopupLayer';
+import Backdrop from '../BackDrop';
+import * as S from './Overlay.styles';
+
+interface OverlayProps {
+  /** 오버레이 열림 상태 */
+  isOpen: boolean;
+  /** 오버레이 닫는 함수 */
+  onClose?: () => void;
+  children?: ReactNode;
+}
+
+interface OverlayContentProps {
+  children: React.ReactNode;
+}
+
+type OverlayComponent = ((props: OverlayProps) => ReactElement | null) & {
+  Content: (props: OverlayContentProps) => ReactElement;
+  Footer: (props: { children: React.ReactNode }) => ReactElement;
+};
+
+const Overlay: OverlayComponent = ({ isOpen, onClose, children }: OverlayProps) => {
+  if (!isOpen) return null;
+
+  return (
+    <PopupRoot>
+      <Backdrop onClick={onClose} />
+      <PopupLayer>{children}</PopupLayer>
+    </PopupRoot>
+  );
+};
+
+const OverlayContent = ({ children }: OverlayContentProps) => {
+  return (
+    <S.Content role="dialog" aria-modal="true">
+      {children}
+    </S.Content>
+  );
+};
+
+const OverlayFooter = ({ children }: { children: React.ReactNode }) => {
+  return <S.Footer>{children}</S.Footer>;
+};
+
+Overlay.Content = OverlayContent;
+Overlay.Footer = OverlayFooter;
+
+export default Overlay;
