8157cb6045c2b7dba075c9ec3b03f37bbb7d910f
임가비 <gabi520@naver.com>
2026-04-06 09:45:00 +0900

    refactor: 클라이언트 컴포넌트 네이밍 규칙 반영 (#10)

--- apps/web/src/app/mypage/_components/BannerContainer/BannerContainer.tsx
+++ apps/web/src/app/mypage/_components/BannerContainer/BannerContainer.tsx
@@ -1,17 +1,17 @@
 import { Suspense } from 'react';
 import BannerFallback from './_components/BannerFallback/BannerFallback';
 import styles from './BannerContainer.module.css';
-import DdayBannerContainer from '@/app/mypage/_components/BannerContainer/_clientBoundary/DdayBannerContainer/DdayBannerContainer';
-import TotalPhotoCountBannerContainer from '@/app/mypage/_components/BannerContainer/_clientBoundary/TotalPhotoCountBannerContainer/TotalPhotoCountBannerContainer';
+import DdayBannerContainerClient from '@/app/mypage/_components/BannerContainer/_clientBoundary/DdayBannerContainerClient/DdayBannerContainerClient';
+import TotalPhotoCountBannerContainerClient from '@/app/mypage/_components/BannerContainer/_clientBoundary/TotalPhotoCountBannerContainerClient/TotalPhotoCountBannerContainerClient';
 
 export default function BannerContainer() {
   return (
     <section className={styles.wrapper}>
       <Suspense fallback={<BannerFallback />}>
-        <DdayBannerContainer />
+        <DdayBannerContainerClient />
       </Suspense>
       <Suspense fallback={<BannerFallback />}>
-        <TotalPhotoCountBannerContainer />
+        <TotalPhotoCountBannerContainerClient />
       </Suspense>
     </section>
   );
