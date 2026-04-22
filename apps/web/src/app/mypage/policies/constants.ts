0dc2c77662decf06b475325b3fc14b4333d86026
임가비 <gabi520@naver.com>
2026-04-23 08:43:14 +0900

    feat: 서비스 이용약관 및 개인정보 처리방침 링크 연결 (#21)

--- apps/web/src/app/mypage/policies/constants.ts
+++ apps/web/src/app/mypage/policies/constants.ts
@@ -1,1 +1,5 @@
 export const PAGE_TITLE = '약관 및 정책';
+export const SERVICE_TERMS_URL =
+  'https://wiry-packet-961.notion.site/LOKIT-34aa07715341808e8ca6c91b339f7637';
+export const PRIVACY_POLICY_URL =
+  'https://wiry-packet-961.notion.site/LOKIT-34aa0771534180fb8534de49503566c9?pvs=74';
--- apps/web/src/app/mypage/policies/page.tsx
+++ apps/web/src/app/mypage/policies/page.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import * as S from './page.style';
-import { PAGE_TITLE } from './constants';
+import { PAGE_TITLE, PRIVACY_POLICY_URL, SERVICE_TERMS_URL } from './constants';
 import CoupleStatusSyncClient from '@/app/mypage/_clientBoundary/CoupleStatusSyncClient/CoupleStatusSyncClient';
 import HeaderClient from '@/app/mypage/policies/HeaderClient/HeaderClient';
 import ChevronRightIcon from '@/assets/images/chevronRight.svg';
+import { useRouter } from 'next/navigation';
 
 export default function PoliciesPage() {
+  const router = useRouter();
+
   return (
     <S.Wrapper>
       <S.SrOnly>{PAGE_TITLE}</S.SrOnly>
@@ -14,13 +17,13 @@ export default function PoliciesPage() {
       <HeaderClient />
       <S.ContentLayout>
         <S.ButtonWrapper>
-          <S.Button>
+          <S.Button onClick={() => router.push(SERVICE_TERMS_URL)}>
             <S.ButtonText>로킷 서비스 이용약관</S.ButtonText>
             <S.ChevronIcon>
               <ChevronRightIcon />
             </S.ChevronIcon>
           </S.Button>
-          <S.Button>
+          <S.Button onClick={() => router.push(PRIVACY_POLICY_URL)}>
             <S.ButtonText>개인정보 처리방침</S.ButtonText>
             <S.ChevronIcon>
               <ChevronRightIcon />
