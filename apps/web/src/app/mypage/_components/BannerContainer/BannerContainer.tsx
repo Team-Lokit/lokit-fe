81ccedf12ca722fe4dc0a2b14004cc3321afb9b1
임가비 <gabi520@naver.com>
2026-04-22 13:57:21 +0900

    feat: 마이페이지 - TotalPhotoCountBannerContainerClient 컴포넌트 제거 (#21)

--- apps/web/src/app/mypage/_components/BannerContainer/BannerContainer.tsx
+++ apps/web/src/app/mypage/_components/BannerContainer/BannerContainer.tsx
@@ -2,7 +2,6 @@ import { Suspense } from 'react';
 import BannerFallback from './_components/BannerFallback/BannerFallback';
 import styles from './BannerContainer.module.css';
 import DdayBannerContainerClient from '@/app/mypage/_components/BannerContainer/_clientBoundary/DdayBannerContainerClient/DdayBannerContainerClient';
-import TotalPhotoCountBannerContainerClient from '@/app/mypage/_components/BannerContainer/_clientBoundary/TotalPhotoCountBannerContainerClient/TotalPhotoCountBannerContainerClient';
 import CustomerSupportSection from '@/app/mypage/_components/BannerContainer/_components/CustomerSupportSection/CustomerSupportSection';
 
 export default function BannerContainer() {
@@ -10,9 +9,6 @@ export default function BannerContainer() {
     <section className={styles.wrapper}>
       <Suspense fallback={<BannerFallback />}>
         <DdayBannerContainerClient />
-      </Suspense>
-      <Suspense fallback={<BannerFallback />}>
-        <TotalPhotoCountBannerContainerClient />
       </Suspense>
       <CustomerSupportSection />
     </section>
