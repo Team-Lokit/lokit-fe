4a681f79acde4463796740f688f9fb8d3b2c6018
임가비 <gabi520@naver.com>
2026-05-11 15:08:35 +0900

    fix: MenuHeader 스타일 수정 (#36)

--- apps/web/src/components/header/menu/MenuHeader.styles.ts
+++ apps/web/src/components/header/menu/MenuHeader.styles.ts
@@ -21,10 +21,10 @@ export const MenuDropdown = styled.div`
   min-width: 120px;
   margin-top: 10px;
   padding: 8px;
-  background: ${({ theme }) => theme.colors.blueWhite.bg8};
+  background: ${({ theme }) => theme.colors.overlay[50]};
   backdrop-filter: blur(25px);
   border: 1px solid ${({ theme }) => theme.colors.blueWhite.border10};
-  border-radius: 16px;
+  border-radius: 20px;
   z-index: 1010;
 `;
 
@@ -34,6 +34,7 @@ export const MenuItem = styled.button<{ variant?: 'default' | 'danger' }>`
   align-items: center;
   width: 100%;
   padding: 12px 16px;
+  border-radius: 12px;
   background: transparent;
   border: none;
   color: ${({ theme, variant }) =>
