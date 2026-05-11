e1976dbc0830e52cc08b7628b6a109cb293f8e35
임가비 <gabi520@naver.com>
2026-05-11 13:20:02 +0900

    fix: CircleButton 스타일 수정 (#36)

--- apps/web/src/components/buttons/circleButton/CircleButton.styles.ts
+++ apps/web/src/components/buttons/circleButton/CircleButton.styles.ts
@@ -7,8 +7,7 @@ export const Wrapper = styled.button`
   width: 44px;
   height: 44px;
   border-radius: 999px;
-  background: ${({ theme }) => theme.colors.blueWhite.bg8};
-  border: 1px solid ${({ theme }) => theme.colors.blueWhite.border10};
+  background: ${({ theme }) => theme.colors.overlay[50]};
   backdrop-filter: ${({ theme }) => theme.effects.backdropBlur[25]};
   color: ${({ theme }) => theme.colors.gray[100]};
 `;
