593a380a4166a9cd3ebf3c34e60988349f56253b
임가비 <gabi520@naver.com>
2026-05-11 10:32:22 +0900

    feat: TextButton small 사이즈 추가 및 medium 사이즈 border-radius 수정 (#36)

--- apps/web/src/components/buttons/textButton/TextButton.styles.ts
+++ apps/web/src/components/buttons/textButton/TextButton.styles.ts
@@ -1,4 +1,4 @@
-import { TextButtonVariant } from '@/types/button.type';
+import { TextButtonSize, TextButtonVariant } from '@/types/button.type';
 import { css, Theme } from '@emotion/react';
 import styled from '@emotion/styled';
 
@@ -13,16 +13,28 @@ const variantStyles = {
     color: ${theme.colors.status.red[200]};
   `,
 };
+
+const sizeStyles = {
+  small: (theme: Theme) => css`
+    ${theme.typography.body15Medium}
+    padding: 6px 12px;
+    border-radius: 8px;
+  `,
+  medium: (theme: Theme) => css`
+    ${theme.typography.body16Semibold}
+    padding: 12px 16px;
+    border-radius: 12px;
+  `,
+};
 export const Wrapper = styled.button<{
+  size: TextButtonSize;
   variant: TextButtonVariant;
   disabled?: boolean;
   textAlign: 'left' | 'center';
 }>`
-  ${({ theme }) => theme.typography.body16Semibold}
-  padding: 12px 16px;
-  border-radius: 8px;
-  text-align: ${({ textAlign }) => textAlign};
+  ${({ size, theme }) => sizeStyles[size](theme)}
   ${({ variant, theme }) => variantStyles[variant](theme)}
+  text-align: ${({ textAlign }) => textAlign};
 
   &:disabled {
     color: ${({ theme }) => theme.colors.gray[600]};
--- apps/web/src/types/button.type.ts
+++ apps/web/src/types/button.type.ts
@@ -2,4 +2,5 @@ export type ButtonSize = 'small' | 'medium' | 'large';
 export type ButtonVariant = 'highlight' | 'primary' | 'secondary' | 'danger';
 export type ChipSize = 'small' | 'medium';
 export type ChipVariant = 'black' | 'white';
+export type TextButtonSize = 'small' | 'medium';
 export type TextButtonVariant = 'primary' | 'default' | 'negative';
